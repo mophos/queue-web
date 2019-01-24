0abcf2dc29ef88ed959938ce5ac8dc2450761ffa
Satit Riapit <siteslave@msn.com>
2019-01-24 17:37:35 +0700

    update for printer/sound

--- src/app/shared/modal-add-priority/modal-add-priority.component.ts
+++ src/app/shared/modal-add-priority/modal-add-priority.component.ts
@@ -24,11 +24,17 @@ export class ModalAddPriorityComponent implements OnInit {
   priorityId: any;
   priorityName: any;
   priorityPrefix: any;
+  prefixes: any = [];
 
   constructor(
     private modalService: NgbModal,
     private alertService: AlertService,
-    private priorityService: PriorityService) { }
+    private priorityService: PriorityService) {
+    this.prefixes = [
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+      'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    ];
+  }
 
   ngOnInit(): void { }
 
