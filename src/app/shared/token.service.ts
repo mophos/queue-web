2f47c85e75e8189e9210f89dc1dbc4cf6d6e6b6b
Satit Riapit <siteslave@msn.com>
2019-02-04 19:00:42 +0700

    fixed generate token

--- src/app/shared/token.service.ts
+++ src/app/shared/token.service.ts
@@ -1,26 +1,37 @@
 import { Injectable, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenService {
 
-  constructor(@Inject('API_URL') private apiUrl: string, private httpClient: HttpClient) { }
+  token: any;
+  httpOptions: any;
+
+  constructor(@Inject('API_URL') private apiUrl: string, private httpClient: HttpClient) {
+    this.token = sessionStorage.getItem('token');
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + this.token
+      })
+    };
+  }
 
   async list() {
     const _url = `${this.apiUrl}/token`;
-    return this.httpClient.get(_url).toPromise();
+    return this.httpClient.get(_url, this.httpOptions).toPromise();
   }
 
   async generate() {
     const _url = `${this.apiUrl}/token`;
-    return this.httpClient.post(_url, {}).toPromise();
+    return this.httpClient.post(_url, {}, this.httpOptions).toPromise();
   }
 
   async remove(token: any) {
     const _url = `${this.apiUrl}/token?token=${token}`;
-    return this.httpClient.delete(_url).toPromise();
+    return this.httpClient.delete(_url, this.httpOptions).toPromise();
   }
 
 }
